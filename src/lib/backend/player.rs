b2339ad98a091b0c14788a0d4a13036457452675
Martin Olsen <Martin.Olsen2@protonmail.com>
2024-12-12 01:50:16 -0600

    Refactored Player Luck Logic.

--- src/lib/backend/player.rs
+++ src/lib/backend/player.rs
@@ -1,15 +1,22 @@
 use super::loadout::Loadout;
-use super::luck_record::{LoadoutLuckRecord, LoadoutPlayerConverter, Luck, PlayerLuckRecord};
-//use super::team::Team;
-//use crate::lib::backend::living_count::LivingCount;
+use super::luck_record::{LoadoutPlayerConverter, PlayerLuckRecord};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Player {
-    pub loadout: Loadout,
-    pub is_alive: bool,
+    loadout: Loadout,
+    is_alive: bool,
 }
 
-impl Player {}
+impl Player {
+    fn make_record_converter(&self) -> LoadoutPlayerConverter {
+        LoadoutPlayerConverter::new(self.is_alive)
+    }
+
+    fn make_player_luck(&self) -> PlayerLuckRecord {
+        self.make_record_converter()
+            .convert(self.loadout.collate_luck())
+    }
+}
 
 impl Default for Player {
     fn default() -> Self {
