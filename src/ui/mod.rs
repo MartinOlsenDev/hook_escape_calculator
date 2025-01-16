c5edd6b795da73029f47809c2addf3c0baeacf2b
Martin Olsen <Martin.Olsen2@protonmail.com>
2025-01-15 22:22:47 -0600

    Added Files Necessary for FLTK

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,1 +1,7 @@
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(clippy::needless_update)]
 
+include!(concat!(env!("OUT_DIR"), "/hook_calculator_ui.rs"));
