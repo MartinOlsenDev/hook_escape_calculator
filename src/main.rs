49651fd4c9d80bd9c13efd1b38fa827fdbc1a56a
Martin Olsen <Martin.Olsen2@protonmail.com>
2025-01-17 16:44:57 -0600

    Squash Me

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,5 @@ mod ui;
 fn main() {
     let app = app::App::default();
     let mut ui = ui::UserInterface::make_window();
-    /*ui.but.set_callback(move |_| {
-        println!("Works!");
-    });*/
     app.run().expect("Internal fltk error encountered.");
 }
