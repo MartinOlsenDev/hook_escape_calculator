13fa46448d5162b9f0b0a0ba00607ed0b657a5a3
Martin Olsen <Martin.Olsen2@protonmail.com>
2025-05-06 15:27:53 -0500

    Column alignment beutification

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -3,7 +3,7 @@ use crate::lib::*;
 use std::borrow::Cow;
 
 use arrayvec::ArrayVec;
-use iced::widget::{checkbox, combo_box, row, text, Column, Text, container, Container};
+use iced::widget::{checkbox, combo_box, container, row, text, Column, Container, Text};
 use iced::Element;
 
 use crate::lib::constants as k;
@@ -46,35 +46,43 @@ impl App {
 
     fn view_team(&self) -> Element<Message> {
         let mut rows = Column::new();
-        let name_header = text("Survivor Name").width(150);
+        let name_header = container(text("Survivor Name")).align_bottom(30).width(125);
         let input_headers = row![
-            text("Slippery Meat").width(200),
-            text("Up the Ante").width(200),
-            text("Offering").width(200),
-            text("Living Status").width(120)
+            container(text("Slippery Meat"))
+                .center_x(200)
+                .align_bottom(30),
+            container(text("Up the Ante"))
+                .center_x(200)
+                .align_bottom(30),
+            container(text("Offering")).center_x(200).align_bottom(30),
+            container(text("Is Dead\nStatus")).center_x(120)
         ];
         let output_headers = row![
-        text("Attempt Chance").width(120),
-        text("Total Chance").width(120)
-        ].width(240);
-        let column_headers = row![
-            name_header,
-            input_headers,
-            output_headers
-        ];
-
+            text("Attempt\nChance").width(120),
+            text("Total\nChance").width(120)
+        ]
+        .width(240);
+        let column_headers = row![name_header, input_headers, output_headers].height(60);
 
         rows = rows.push(column_headers);
 
         for player_id in 0..k::TEAM_MAX_CAPACITY {
-            let player_name = text(format!("Player {}", player_id + 1)).width(150);
+            let player_name = text(format!("Player {}", player_id + 1)).width(125);
             let row_input = self.make_player(player_id);
             let (attempt_chance, total_chance) = self.widgets.odds.get(player_id).expect(
                 "Generated id in range 0..TEAM_MAX_CAPACITY always less than TEAM_MAX_CAPACITY.",
             );
             let row_output = row![
-                text(attempt_chance.to_owned()).width(120),
-                text(total_chance.to_owned()).width(120)
+                container(
+                    container(container(text(attempt_chance.to_owned())).align_left(110))
+                        .align_right(120)
+                )
+                .width(120),
+                container(
+                    container(container(text(total_chance.to_owned())).align_left(80))
+                        .align_right(100)
+                )
+                .width(120)
             ];
             let row = row![player_name, row_input, row_output];
             rows = rows.push(row)
@@ -88,31 +96,47 @@ impl App {
         );
 
         row![
-            container(combo_box(
-                &self.widgets.tier_choices,
-                "",
-                Some(&TierSlotDisplay(player.get_slippery_tier())),
-                move |TierSlotDisplay(x)| {
-                    Message::UpdateSurvivor(SurvivorUpdate::slippery(id, x))
-                }
-            ).width(120)).width(200),
-            container(combo_box(
-                &self.widgets.tier_choices,
-                "",
-                Some(&TierSlotDisplay(player.get_uta_tier())),
-                move |TierSlotDisplay(x)| { Message::UpdateSurvivor(SurvivorUpdate::uta(id, x)) }
-            ).width(120)).width(200),
-            container(combo_box(
-                &self.widgets.offering_choices,
-                "",
-                Some(&OfferingSlotDisplay(player.get_offering())),
-                move |OfferingSlotDisplay(x)| {
-                    Message::new_surv_update(id, SurvivorUpdateData::Offering(x))
-                }
-            ).width(150)).width(200),
-            checkbox("is dead", player.is_dead())
-                .on_toggle(move |x| Message::new_surv_update(id, SurvivorUpdateData::Life(x)))
+            container(
+                combo_box(
+                    &self.widgets.tier_choices,
+                    "",
+                    Some(&TierSlotDisplay(player.get_slippery_tier())),
+                    move |TierSlotDisplay(x)| {
+                        Message::UpdateSurvivor(SurvivorUpdate::slippery(id, x))
+                    }
+                )
                 .width(120)
+            )
+            .center_x(200),
+            container(
+                combo_box(
+                    &self.widgets.tier_choices,
+                    "",
+                    Some(&TierSlotDisplay(player.get_uta_tier())),
+                    move |TierSlotDisplay(x)| {
+                        Message::UpdateSurvivor(SurvivorUpdate::uta(id, x))
+                    }
+                )
+                .width(120)
+            )
+            .center_x(200),
+            container(
+                combo_box(
+                    &self.widgets.offering_choices,
+                    "",
+                    Some(&OfferingSlotDisplay(player.get_offering())),
+                    move |OfferingSlotDisplay(x)| {
+                        Message::new_surv_update(id, SurvivorUpdateData::Offering(x))
+                    }
+                )
+                .width(150)
+            )
+            .center_x(200),
+            container(
+                checkbox("", player.is_dead())
+                    .on_toggle(move |x| Message::new_surv_update(id, SurvivorUpdateData::Life(x)))
+            )
+            .center_x(120)
         ]
         .into()
     }
